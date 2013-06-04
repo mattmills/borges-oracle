dd81ba5b51e8fdad0097c66178f44a1778048c52
Matt Mills <sunrisetimes@gmail.com>
2013-06-04 12:15:56 +0100

    the markov part is done (but slow)

--- borges_oracle.rb
+++ borges_oracle.rb
@@ -5,7 +5,8 @@ Bundler.require
 class BorgesOracle < Sinatra::Base
   get '/' do
     markov = MarkyMarkov::Dictionary.new('dictionary')
-    story = (1..2).map{ markov.generate_n_sentences 7 }
-    haml :index, :locals => story
+    story = (1..5).map{ markov.generate_n_sentences 7 }
+    title = markov.generate_n_words 3
+    haml :index, :locals => {:story => story, :title => title}
   end
 end
